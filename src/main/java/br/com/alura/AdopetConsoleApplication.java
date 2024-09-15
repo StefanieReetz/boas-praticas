d3a53072e2ddf86830e8d96bd130a3c533a2364e
StefanieReetz <stefaniebeatrizreetz@gmail.com>
2024-09-14 21:39:19 -0300

    finalizando mais um codigo, melhorando e aprendendo sobre o switch case

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -1,51 +1,53 @@
 package br.com.alura;
 
-import br.com.alura.client.ClientHttpConfiguration;
-import br.com.alura.service.AbrigoService;
-import br.com.alura.service.PetService;
 import java.util.Scanner;
 
 public class AdopetConsoleApplication {
 
     public static void main(String[] args) {
-        ClientHttpConfiguration client = new ClientHttpConfiguration();
-        PetService petService = new PetService(client);
-        AbrigoService abrigoService = new AbrigoService(client);
+
+        CommandExecutor executor = new CommandExecutor();
 
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
         try {
             int opcaoEscolhida = 0;
             while (opcaoEscolhida != 5) {
-                System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
-                System.out.println("1 -> Listar abrigos cadastrados");
-                System.out.println("2 -> Cadastrar novo abrigo");
-                System.out.println("3 -> Listar pets do abrigo");
-                System.out.println("4 -> Importar pets do abrigo");
-                System.out.println("5 -> Sair");
+                exibeMenu();
 
                 String textoDigitado = new Scanner(System.in).nextLine();
                 opcaoEscolhida = Integer.parseInt(textoDigitado);
 
-                if (opcaoEscolhida == 1) {
-                    abrigoService.listarAbrigo();
-                } else if (opcaoEscolhida == 2) {
-                abrigoService.cadastrarAbrigo();
-                }else if (opcaoEscolhida == 3) {
-                    petService.listarPetsDoAbrigo();
-                } else if (opcaoEscolhida == 4) {
-                petService.importarPetsDoAbrigo();
-                } else if (opcaoEscolhida == 5) {
-                    break;
-                } else {
-                    System.out.println("NÚMERO INVÁLIDO!");
-                    opcaoEscolhida = 0;
+                switch (opcaoEscolhida) {
+                    case 1 -> executor.executeCommand(new ListarAbrigoCommand());
+                    case 2 -> executor.executeCommand(new CadastrarAbrigoCommand());
+                    case 3 -> executor.executeCommand(new ListarPetsDoAbrigoCommand());
+                    case 4 -> executor.executeCommand(new ImportarPetsDoAbrigoCommand());
+                    case 5 -> System.exit(0);
+                    default -> opcaoEscolhida = 0;
                 }
+//                switch (opcaoEscolhida){
+//                    case 1:
+//                        executor.executeCommand(new ListarAbrigoCommand());
+//                        break;
+//                    case 2:
+//                        executor.executeCommand(new CadastrarAbrigoCommand());
+//                        break;
+//                    case 3:
+//                        executor.executeCommand(new ListarPetsDoAbrigoCommand());
+//                        break;
+//                    case 4:
+//                        executor.executeCommand(new ImportarPetsDoAbrigoCommand());
+//                        break;
+//                    case 5:
+//                        break;
+//                    default:
+//                        System.out.println("NÚMERO INVÁLIDO!");
+//                        opcaoEscolhida = 0;
+//                        break;
+//                }
+                System.out.println("Finalizando o programa...");
+
             }
-            System.out.println("Finalizando o programa...");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 //    private static HttpResponse<String> dispararRequisicaoGet(HttpClient client, String uri) throws IOException, InterruptedException {
 ////        HttpClient client = HttpClient.newHttpClient();
 ////        String uri = "http://localhost:8080/abrigos";
@@ -67,5 +69,18 @@ public class AdopetConsoleApplication {
 //        return client.send(request, HttpResponse.BodyHandlers.ofString());
 //
 //    }
-}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
+    }
+
+    private static void exibeMenu() {
+        System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+        System.out.println("1 -> Listar abrigos cadastrados");
+        System.out.println("2 -> Cadastrar novo abrigo");
+        System.out.println("3 -> Listar pets do abrigo");
+        System.out.println("4 -> Importar pets do abrigo");
+        System.out.println("5 -> Sair");
+    }
+}
---
+++ src/main/java/br/com/alura/ListarAbrigoCommand.java
@@ -0,0 +1,22 @@
+package br.com.alura;
+
+import br.com.alura.client.ClientHttpConfiguration;
+import br.com.alura.service.AbrigoService;
+import br.com.alura.service.PetService;
+
+import java.io.IOException;
+
+public class ListarAbrigoCommand implements Command {
+
+    @Override
+    public void execute() {
+        try {
+            ClientHttpConfiguration client = new ClientHttpConfiguration();
+            AbrigoService abrigoService = new AbrigoService(client);
+
+            abrigoService.listarAbrigo();
+        }catch (IOException | InterruptedException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+}
